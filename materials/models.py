b06fd985cb93431b92e227eb91283d2b32ff69f0
Julia Adonina <julie014@mail.ru>
2025-06-08 18:45:28 +0300

    viewsets, generics

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название",
+        help_text="Введите название курса.",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Введите описание курса.",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="images/",
+        verbose_name="Превью (картинка)",
+        help_text="Загрузите превью (картинку) курса.",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=250,
+        verbose_name="Название",
+        help_text="Введите название урока.",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+        help_text="Укажите описание урока",
+        blank=True,
+        null=True,
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="Курс",
+    )
+
+    image = models.ImageField(
+        upload_to="images/",
+        verbose_name="Превью(картинка)",
+        help_text="Загрузите превью(картинку) урока.",
+        blank=True,
+        null=True,
+    )
+    video_url = models.URLField(
+        verbose_name="Ссылка на видео",
+        help_text="Укажите ссылку на видео.",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "урок"
+        verbose_name_plural = "уроки"
+        ordering = ["name", "course"]
+
+    def __str__(self):
+        return f"{self.name} {self.course}."
